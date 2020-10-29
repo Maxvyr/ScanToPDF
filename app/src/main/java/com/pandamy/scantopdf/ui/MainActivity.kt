8657dccefaadc0801a1d452948a9aa9b83578782
maxvyr <maxvy_@naver.com>
2020-10-29 16:30:55 +0100

    add CameraX #1

--- app/src/main/java/com/pandamy/scantopdf/ui/MainActivity.kt
+++ app/src/main/java/com/pandamy/scantopdf/ui/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.pandamy.scantopdf.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.pandamy.scantopdf.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createScanFab.setOnClickListener { openCamera() }
+    }
+
+    private fun openCamera() {
+        intent = Intent(this,ScanActivity::class.java)
+        startActivity(intent)
     }
 }
