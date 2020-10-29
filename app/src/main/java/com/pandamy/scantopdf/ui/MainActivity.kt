091304dfd3eec37835c8a0614f907b961742eb07
maxvyr <maxvy_@naver.com>
2020-10-29 12:06:16 +0100

    add function open camera

--- app/src/main/java/com/pandamy/scantopdf/ui/MainActivity.kt
+++ app/src/main/java/com/pandamy/scantopdf/ui/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.pandamy.scantopdf.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import com.pandamy.scantopdf.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        createScanFab.setOnClickListener { it -> openCamera() }
+    }
+
+    private fun openCamera() {
+        intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE_SECURE)
+        startActivity(intent)
     }
 }
