5b17ec8a0f6c060af1bbd75d2908813dd67127ee
maxvyr <maxvy_@naver.com>
2020-11-01 13:00:31 +0100

    save image

--- app/src/main/java/com/pandamy/scantopdf/ui/MainActivity.kt
+++ app/src/main/java/com/pandamy/scantopdf/ui/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.pandamy.scantopdf.ui
 
 import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.util.Log
 import com.pandamy.scantopdf.R
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,11 +14,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        createScanFab.setOnClickListener { it -> openCamera() }
+        createScanFab.setOnClickListener { openCamera() }
     }
 
     private fun openCamera() {
         intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE_SECURE)
-        startActivity(intent)
+        startActivityForResult(intent, CAMERA_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CAMERA_REQUEST) {
+            Log.d(TAG, "onActivityResult: return")
+            val bitmap : Bitmap = data?.extras?.get("data") as Bitmap
+            imageViewTest.setImageBitmap(bitmap)
+        }
+    }
+
+    companion object {
+        val TAG: String = MainActivity::class.java.simpleName
+        const val CAMERA_REQUEST = 42
     }
 }
