dfc6a391977e768796712b5387f0e8c0ed7710c8
maxvyr <maxvy_@naver.com>
2020-11-05 15:04:08 +0100

    add room to dataclass

--- app/src/main/java/com/pandamy/scantopdf/models/PhotoForScanning.kt
+++ app/src/main/java/com/pandamy/scantopdf/models/PhotoForScanning.kt
@@ -1,8 +1,14 @@
 package com.pandamy.scantopdf.models
 
 import android.graphics.Bitmap
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
+@Entity(tableName = "photoScanning")
 data class PhotoForScanning(
+    @PrimaryKey
     var id : Long,
+    @ColumnInfo(name = "photo_scan")
     var picture: Bitmap
 )
